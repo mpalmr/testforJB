f7d038bd7bb7b50b1ae808df6b2a2e52baebeed8
Matt Palmer <matt@silvercreekit.com>
2017-05-12 18:45:06 -0400

    Styles

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,16 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 const Clean = require('clean-webpack-plugin');
 const Html = require('html-webpack-plugin');
+const ExtractText = require('extract-text-webpack-plugin');
+const autoprefixer = require('autoprefixer');
 const pkg = require('./package.json');
 
 const dir = {
   src: path.resolve('src'),
   dist: path.resolve('dist'),
 };
+
+const styleBundle = new ExtractText(`${pkg.name}.css`);
 
 const base = {
   context: dir.src,
@@ -17,14 +21,32 @@ const base = {
     modules: [dir.src, 'node_modules'],
   },
   module: {
-    rules: [{
-      test: /\.js$/,
-      use: {
-        loader: 'babel-loader',
-        options: { cacheDirectory: true },
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: { cacheDirectory: true },
+        },
       },
-    }],
+      {
+        test: /\.scss$/,
+        use: styleBundle.extract([
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ]),
+      },
+    ],
   },
+  plugins: [
+    styleBundle,
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        postcss: () => [autoprefixer(pkg.browserslist)],
+      },
+    }),
+  ],
 };
 
 const dev = {
